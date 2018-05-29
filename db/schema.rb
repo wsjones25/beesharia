4304bc6e20bc4ca10a657b72c6a548b9b6621c40
Will Jones <wsjones1101@gmail.com>
2018-05-29 19:14:10 +0100

    form css improvements

--- db/schema.rb
+++ db/schema.rb
@@ -73,7 +73,7 @@ ActiveRecord::Schema.define(version: 2018_05_29_115031) do
     t.inet "last_sign_in_ip"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.boolean "company", default: false
+    t.boolean "company"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
