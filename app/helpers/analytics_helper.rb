e84b8b0d52774eb704d50e276762cf140c042bd3
Owen Prew <odprew@gmail.com>
2018-05-31 17:02:46 +0100

    changes

--- app/helpers/analytics_helper.rb
+++ app/helpers/analytics_helper.rb
@@ -1,22 +1,21 @@
 module AnalyticsHelper
-  def balance
-    topups = current_user.topups.map(&:amount).sum
-    investments = current_user.investments.map(&:credit_amount).sum
-    @balance = topups - investments
-  end
+  # def balance
+  #   topups = current_user.topups.map(&:amount).sum
+  #   investments = current_user.investments.map(&:credit_amount).sum
+  #   @balance = topups - investments
+  # end
 
-  def total_invested
-    investments = current_user.investments.map(&:credit_amount).sum
-  end
+  # def total_invested
+  #   investments = current_user.investments.map(&:credit_amount).sum
+  # end
 
-  def avg_expected_return
-    investments = current_user.investments.map(&:credit_amount).sum
-    arr = []
-    current_user.investments.each do |investment|
-      arr << investment.credit_amount * Company.find(investment.company_id).loan_rate
-    end
-    total_return = arr.reduce(0) {|sum, num| sum + num }
-    portfolio_return = (total_return/investments)/100
-  end
-
+  # def avg_expected_return
+  #   investments = current_user.investments.map(&:credit_amount).sum
+  #   arr = []
+  #   current_user.investments.each do |investment|
+  #     arr << investment.credit_amount * Company.find(investment.company_id).loan_rate
+  #   end
+  #   total_return = arr.reduce(0) {|sum, num| sum + num }
+  #   portfolio_return = (total_return/investments)/100
+  # end
 end
