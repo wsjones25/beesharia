4da2297b1d857d3977501c8366dea51e93596c83
Hassan Daher <hassanydaher@gmail.com>
2018-06-04 15:14:20 +0100

    fixed expected returns method in the Analytics Helper controller

--- app/helpers/analytics_helper.rb
+++ app/helpers/analytics_helper.rb
@@ -2,7 +2,7 @@ module AnalyticsHelper
   def balance
     topups = current_user.topups.map(&:amount).sum
     investments = current_user.investments.map(&:credit_amount).sum
-    @balance = topups - investments
+    balance = topups - investments
   end
 
   def total_invested
@@ -10,24 +10,41 @@ module AnalyticsHelper
   end
 
   def avg_expected_return
-    investments = current_user.investments.map(&:credit_amount).sum
+    total_investments = current_user.investments.map(&:credit_amount).sum
     arr = []
-    current_user.investments.each do |investment|
-      arr << investment.credit_amount * Company.find(investment.company_id).loan_rate
+    if total_investments == 0
+      return 0
+    else
+      current_user.investments.each do |investment|
+        arr << investment.credit_amount.to_f / total_investments.to_f * investment.company.loan_rate.to_f
+      end
+      return arr.sum.round(2)
     end
-    total_return = arr.reduce(0) {|sum, num| sum + num }
-    portfolio_return = (total_return/investments)/100
+
   end
-
-
-  def avg_expected_return
-    investments = current_user.investments.map(&:credit_amount).sum
-    arr = []
-    current_user.investments.each do |investment|
-      arr << investment.credit_amount * Company.find(investment.company_id).loan_rate
-    end
-    total_return = arr.reduce(0) {|sum, num| sum + num }
-    portfolio_return = (total_return/investments)/100
-  end
-
 end
+
+    # arr = []
+    # current_user.investments.each do |investment|
+    #   arr << investment.credit_amount * Company.find(investment.company_id).loan_rate
+    # end
+    # total_return = arr.reduce(0) {|sum, num| sum + num }
+    # if investments == 0
+    #   portfolio_return = 0
+    # else
+    #   portfolio_return = ( total_return / investments ) *100
+    # end
+
+
+
+  # def avg_expected_return
+  #   investments = current_user.investments.map(&:credit_amount).sum
+  #   arr = []
+  #   current_user.investments.each do |investment|
+  #     arr << investment.credit_amount * Company.find(investment.company_id).loan_rate
+  #   end
+  #   total_return = arr.reduce(0) {|sum, num| sum + num }
+  #   portfolio_return = (total_return/investments)/100
+  # end
+
+
