690413086eca34a761bc48012f400c62900ab75a
Owen Prew <odprew@gmail.com>
2018-06-05 16:52:58 +0100

    touching up

--- app/javascript/components/banner.js
+++ app/javascript/components/banner.js
@@ -5,7 +5,7 @@ function loadDynamicBannerText() {
   const banner = document.getElementById('banner-typed-text')
   if (banner) {
     new Typed('#banner-typed-text', {
-      strings: ["Ethical investing", "FAC regulated", "Do something interesting with your money"],
+      strings: ["Ethical investing", "FCA regulated", "Do something interesting with your money"],
       typeSpeed: 50,
       loop: true
     });
